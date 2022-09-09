42ea487107c9941de49aad4cf2ac86642d5976f7
ESTHEREKANEM <estekanem1@gmail.com>
2022-09-09 12:25:09 -0700

    print numbers

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -4,14 +4,16 @@
  *
  * Description: using the main function
  * this program prints "Programming is positive, zero, or negative
- * Return: Always 0
+ * Return: 0
  */
 int main(void)
 {
 int n;
+
 for (n = '0'; n <= '9'; n++)
 {
 	putchar(n);
 }
 putchar('\n');
 return (0);
+}
