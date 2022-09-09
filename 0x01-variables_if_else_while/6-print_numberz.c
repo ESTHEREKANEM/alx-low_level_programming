ab635cfb499502300968f5f8c83ed2b988ca9bc6
ESTHEREKANEM <estekanem1@gmail.com>
2022-09-09 11:44:54 -0700

    print numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,15 +1,17 @@
+#include <stdio.h>
 /**
- * main - Prints all single digit number
+ * main - print if the number is postive, zero, or negative
  *
- * Return: Always (Success)
- */ 
+ * Description: using the main function
+ * this program prints "Programming is positive, zero, or negative
+ * Return: 0
+ */
 int main(void)
 {
 int n;
 for (n = '0'; n <= '9'; n++)
 {
-putchar(n);
+	putchar(n);
 }
 putchar('\n');
 return (0);
-}
