cde231d82ed3ab6fd779d8202bdc8f3c46666fd8
ESTHEREKANEM <estekanem1@gmail.com>
2022-09-13 15:38:14 -0700

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -19,14 +19,15 @@ void times_table(void)
 
 			prod = num * multi;
 
-		if (prod <= 9)
-			_putchar(' ');
-		else
-			_putchar((prod / 10) + '0');
+			if (prod <= 9)
+				_putchar(' ');
+			else
+				_putchar((prod / 10) + '0');
 
 			_putchar((prod % 10) + '0');
 		}
 		_putchar('\n');
+
 	}
 }
 
