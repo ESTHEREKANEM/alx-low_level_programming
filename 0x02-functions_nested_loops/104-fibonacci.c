2413e59fdc7feaffdb2bceebff7398317c145bef
ESTHEREKANEM <estekanem1@gmail.com>
2022-09-14 16:13:56 -0700

    Hot fix

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,22 +7,29 @@
  */
 int main(void)
 {
-long int i, j, k, next;
+unsigned long int i, j, k, j1, j2, k1, k2;
 j = 1;
 k = 2;
-for (i = 1; i <= 50; ++i)
+printf("%lu", j);
+for (i = 1; i < 91; i++)
 {
-if (j != 139583862445)
+printf(", %lu", k);
+k = k + j;
+j = k - j;
+}
+j1 = j / 1000000000;
+j2 = j % 1000000000;
+k1 = k / 1000000000;
+k2 = k % 1000000000;
+for (i = 92; i < 99; ++i)
 {
-printf("%ld, ", j);
+printf(", %lu", k1 + (k2 / 1000000000));
+printf("%lu", k2 % 1000000000);
+k1 = k1 + j1;
+j1 = k1 - j1;
+k2 = k2 + j2;
+j2 = k2 - j2;
 }
-else
-{
-printf("%ld\n", j);
-}
-next = j + k;
-j = k;
-k = next;
-}
+printf("\n");
 return (0);
 }
