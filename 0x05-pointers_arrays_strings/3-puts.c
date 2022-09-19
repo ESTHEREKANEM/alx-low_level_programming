50e3d90795ef61fc52b7ca04869e6af06a19a75c
ESTHEREKANEM <estekanem1@gmail.com>
2022-09-19 12:02:12 -0700

    Task 2 - 4

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts - function prints a string
+ * @str: string to print
+ *
+ */
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (*(str + i) != '\0')
+	{
+		putchar(*(str + i));
+		i++;
+	}
+	putchar(10);
+}
