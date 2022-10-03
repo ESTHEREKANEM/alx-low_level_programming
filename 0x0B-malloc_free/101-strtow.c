04454b7dfec346440c1ec220f574a705a83c3c3c
ESTHEREKANEM <estekanem1@gmail.com>
2022-10-03 14:09:22 -0700

    Task 6

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -51,7 +51,6 @@ char **strtow(char *str)
 		{
 			if (str[c] == ' ')
 				a1++;
-
 			if (str[c] != ' ' && (str[c + 1] == ' ' || str[c + 1] == '\0'))
 			{
 				aout[i] = malloc((c - a1 + 2) * sizeof(char));
